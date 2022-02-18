e06edd163b449aa29bc724aaf4d2cc863b370329
sharandoss <90198712+sharandoss@users.noreply.github.com>
2022-02-17 22:56:58 -0500

    Assignment 3

---
+++ Assignment 3/Multiscreen/Multiscreen/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Multiscreen
+//
+//  Created by Sharan Mohanadoss on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("This is home screen")
+                .tabItem {
+                    Text("Home")
+                    Image(systemName: "house.fill")
+                  }
+            Text("This is about us screen")
+                .tabItem {
+                    Text("About Us")
+                    Image(systemName: "person.fill")
+                }
+            Text("This is contct us screen")
+                .tabItem {
+                    Text("Contact Us")
+                    Image(systemName: "phone.fill")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Assignment 3/Multiscreen/Multiscreen/MultiscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiscreenApp.swift
+//  Multiscreen
+//
+//  Created by Sharan Mohanadoss on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
